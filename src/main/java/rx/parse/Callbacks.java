73e76abebbc859f38ef60427f0bc4610bc655514
Andrew Chen <yongjhih@gmail.com>
2015-03-22 00:49:48 +0800

    Remove unnecessary interfaces

--- src/main/java/rx/parse/Callbacks.java
+++ src/main/java/rx/parse/Callbacks.java
@@ -1,91 +1,58 @@
 package rx.parse;
 
 import com.parse.*;
-/*
-import com.parse.FindCallback;
-import com.parse.GetCallback;
-import com.parse.SaveCallback;
-import com.parse.ParseObject;
-import com.parse.ParseException;
-*/
+import rx.functions.*;
 
 import java.util.List;
 
 /**
- * getInBackground(GetCallback callback)
- * findInBackground(FindCallback callback) // abstract class
- * findInBackground((list, e) -> {;})
- * findInBackground(CallbackUtils.create((list, e) -> {} ))
+ * For lambda
  */
 public class Callbacks {
-    public interface IFindCallback<E> {
-        void done(List<E> list, ParseException e);
-    }
-
-    public interface IGetCallback<E> {
-        void done(E emit, ParseException e);
-    }
-
-    public interface ISaveCallback {
-        void done(ParseException e);
-    }
-
-    public interface ICountCallback {
-        void done(int count, ParseException e);
-    }
-
-    public interface ILogInCallback {
-        void done(ParseUser user, ParseException e);
-    }
-
-    public interface IFunctionCallback<E> {
-        void done(E emit, ParseException e);
-    }
-
-    public static <T extends ParseObject> FindCallback<T> find(IFindCallback<T> callback) {
+    public static <T extends ParseObject> FindCallback<T> find(Action2<List<T>, ? super ParseException> callback) {
         return new FindCallback<T>() {
             @Override public void done(List<T> list, ParseException e) {
-                callback.done(list, e);
+                callback.call(list, e);
             }
         };
     }
 
-    public static <T extends ParseObject> GetCallback<T> get(IGetCallback<T> callback) {
+    public static <T extends ParseObject> GetCallback<T> get(Action2<? super T, ? super ParseException> callback) {
         return new GetCallback<T>() {
             @Override public void done(T emit, ParseException e) {
-                callback.done(emit, e);
+                callback.call(emit, e);
             }
         };
     }
 
-    public static SaveCallback save(ISaveCallback callback) {
+    public static SaveCallback save(Action1<? super ParseException> callback) {
         return new SaveCallback() {
             @Override public void done(ParseException e) {
-                callback.done(e);
+                callback.call(e);
             }
         };
     }
 
-    public static CountCallback count(ICountCallback callback) {
+    public static CountCallback count(Action2<? super Integer, ? super ParseException> callback) {
         return new CountCallback() {
             @Override public void done(int count, ParseException e) {
-                callback.done(count, e);
+                callback.call(count, e);
             }
         };
     }
 
-    public static LogInCallback login(ILogInCallback callback) {
+    public static LogInCallback login(Action2<? super ParseUser, ? super ParseException> callback) {
         return new LogInCallback() {
             @Override public void done(ParseUser user, ParseException e) {
-                callback.done(user, e);
+                callback.call(user, e);
             }
         };
     }
 
-    public static <T> FunctionCallback function(IFunctionCallback<T> callback) {
-        return new FunctionCallback<T>() {
-            @Override public void done(T emit, ParseException e) {
-                callback.done(emit, e);
+    public static <R> FunctionCallback function(Action2<? super R, ? super ParseException> callback) {
+        return new FunctionCallback<R>() {
+            @Override public void done(R emit, ParseException e) {
+                callback.call(emit, e);
             }
         };
     }
